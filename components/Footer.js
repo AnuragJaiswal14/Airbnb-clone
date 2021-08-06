ca2b13bb9d9d952b89b5f5f31b08ee7755fbc7d7
AnuragJaiswal14 <anujaiswal9087@gmail.com>
2021-08-06 22:21:25 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 p4-14 bg-gray-100 text-gray-600 ">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold"> ABOUTS </h5>
+        <p> How AirBnB works </p>
+
+        <p> New room</p>
+        <p>Investors</p>
+        <p> AirBnb Plus</p>
+        <p>AirBnB Luxury Time</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold"> COMMUNITY </h5>
+        <p> How AirBnB works </p>
+
+        <p> Accesiblity</p>
+        <p>This is a Dummy site</p>
+        <p>Yes You can call it a preety awesome clone</p>
+        <p>Referrals accepted</p>
+        <p> AJ on the toes</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold"> HOST </h5>
+        <p> AJ </p>
+        <p> Along with some skills in </p>
+        <p>React </p>
+        <p> React native</p>
+        <p>TailwindCSS </p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold"> SUPPORT </h5>
+        <p> Help Centre</p>
+
+        <p> Trust & Safety</p>
+        <p>Hello to visitors</p>
+        <p>Do check my Github</p>
+        <p>E-mail for help</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
